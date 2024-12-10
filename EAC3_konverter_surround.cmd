fc9399bd8b9b20c5c383fb62e12f77106c35f829
djnedrelid <thronic@gmail.com>
2024-12-10 21:13:32 +0100

    Oppdatering

--- EAC3_konverter_surround.cmd
+++ EAC3_konverter_surround.cmd
@@ -16,8 +16,8 @@ echo Hva er formatet? mp4, mkv, etc.
 echo.
 set /p ekstensjon=:
 
-:: Konverter kun første spor og kopier resten som de er, siden dette er til surround og kanskje noen er stereo.
-ffmpeg -i %sti% -map 0:v -map 0:a -map 0:s? -c:v copy -c:s copy -c:a:0 eac3 -b:a 1536k -ac 6 -metadata:s:a:0 title="Dolby Digital+ 5.1" -c:a copy %sti%.tmp.%ekstensjon%
+:: Konverter kun første spor.
+ffmpeg -i %sti% -map 0:v -map 0:a:0 -map 0:s? -c:v copy -c:s copy -c:a:0 eac3 -b:a 1536k -ac 6 -metadata:s:a:0 title="Dolby Digital+ 5.1" %sti%.tmp.%ekstensjon%
 move /y %sti%.tmp.%ekstensjon% %sti%
 
 echo Ferdig ...
--- README.md
+++ README.md
@@ -4,5 +4,8 @@
 Noen skripter til konvertering av lyd til AC3 2.0 og EAC3 5.1  
 320kbps per kanal for AC3 stereo (640 for 2 kanaler)  
 256kbps per kanal for E-AC3 surround (1536 for 6(5.1) kanaler).  
+  
+Stereo skriptene tar med og konverterer alt, siden kilde kan være så mange kanaler de vil.  
+Surround skriptene tar med og konverterer kun første spor, i tilfelle noen er stereo fra før.  
 
 *Extract ffmpeg.rar before using any script (archived for github size limitations).*
